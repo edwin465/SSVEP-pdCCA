6769a6cc5a6b2b986aef8488748bc38f1b3cbbff
Chi Man Wong <chiman465@gmail.com>
2023-11-13 18:10:18 +0800

    Update load_mat_2021.m

--- load_mat_2021.m
+++ load_mat_2021.m
@@ -5,15 +5,29 @@
 close all
 clear all
 filename=mfilename('fullpath');
-for sn=1:8
-    if sn==3
-        K=2;
+dataset_no=2; % 1: offline exp., 2: online exp.
+if dataset_no==1
+    N_s=8;
+else
+    N_s=12;
+end
+for sn=1:N_s
+    if dataset_no==1
+       if sn==3 
+          K=2;
+       else
+          K=3;
+       end
     else
-        K=3;
+        K=2;
     end
     data=zeros(9,1035,160,K);
     for block_no=1:K
-        loaddata=load([cd '\S' num2str(sn) '\' num2str(block_no) '.mat']);
+        if dataset_no==1
+           loaddata=load([cd '\S' num2str(sn) '\' num2str(block_no) '.mat']);
+        else
+           loaddata=load([cd '\SS' num2str(sn) '\' num2str(block_no) '.mat']);
+        end
         data1 = permute(loaddata.EEG_downsample,[2 3 1]);
         data(:,:,:,block_no)=data1;
     end
